f775c7e4b2374aa90e69aa552b9647855e823a3c
nika jashi <jashi.nika1@gmail.com>
2023-11-01 15:31:29 +0400

    now everything including docker works perfectly

--- application/core/settings.py
+++ application/core/settings.py
@@ -6,7 +6,7 @@ import dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-dotenv.load_dotenv(os.path.join(BASE_DIR, ".env"))
+dotenv.load_dotenv(os.path.join(BASE_DIR, "../.env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Running makemigrations"
-python manage.py makemigrations
+python application/manage.py makemigrations
 
 echo "Running migrations"
-python manage.py migrate
+python application/manage.py migrate
 
-python manage.py collectstatic --noinput
+python application/manage.py collectstatic --noinput
 
 echo "Running server"
-python manage.py runserver 127.0.0.1:8000
+python application/manage.py runserver 0.0.0.0:8000
